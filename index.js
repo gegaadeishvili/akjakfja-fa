ba1a33acbc1b6694f375be58e3578f212569b2da
gegaadeishvili <128076325+gegaadeishvili@users.noreply.github.com>
2024-04-02 03:24:50 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const myButton = document.getElementById('myButton');
+myButton.addEventListener('click', function() {
+    window.location.href = 'other.html'; 
+});
+
+const myButton2 = document.getElementById('myButton2');
+myButton2.addEventListener('click', function() {
+    window.location.href = 'other2.html'; 
+});
+
+const myButton3 = document.getElementById('myButton3');
+myButton3.addEventListener('click', function() {
+    window.location.href = 'other3.html'; 
+});
+
+
+const myButton4 = document.getElementById('myButton4');
+myButton4.addEventListener('click', function() {
+    window.location.href = 'other4.html'; 
+});
+
